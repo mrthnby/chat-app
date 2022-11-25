d86961fb49b1c9b255dfe65e401061d5a25ae0b9
mrthnby <meralihanbay@gmail.com>
2022-11-25 22:06:02 +0300

    message ui design issues fixed

--- lib/widgets/message_box.dart
+++ lib/widgets/message_box.dart
@@ -24,19 +24,21 @@ class MessageBox extends StatelessWidget {
               crossAxisAlignment: CrossAxisAlignment.end,
               mainAxisAlignment: MainAxisAlignment.end,
               children: [
-                Container(
-                  decoration: const BoxDecoration(
-                      color: Colors.black54,
-                      borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(12),
-                        topRight: Radius.circular(12),
-                        bottomLeft: Radius.circular(12),
-                      )),
-                  child: Padding(
-                    padding: const EdgeInsets.all(10.0),
-                    child: Text(
-                      message.content,
-                      style: const TextStyle(color: Colors.white),
+                Flexible(
+                  child: Container(
+                    decoration: const BoxDecoration(
+                        color: Colors.black54,
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(12),
+                          topRight: Radius.circular(12),
+                          bottomLeft: Radius.circular(12),
+                        )),
+                    child: Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Text(
+                        message.content,
+                        style: const TextStyle(color: Colors.white),
+                      ),
                     ),
                   ),
                 ),
@@ -67,19 +69,21 @@ class MessageBox extends StatelessWidget {
                     fontSize: 9,
                   ),
                 ),
-                Container(
-                  decoration: BoxDecoration(
-                      color: Colors.deepPurple.shade700,
-                      borderRadius: const BorderRadius.only(
-                        topLeft: Radius.circular(12),
-                        topRight: Radius.circular(12),
-                        bottomRight: Radius.circular(12),
-                      )),
-                  child: Padding(
-                    padding: const EdgeInsets.all(10.0),
-                    child: Text(
-                      message.content,
-                      style: const TextStyle(color: Colors.white),
+                Flexible(
+                  child: Container(
+                    decoration: BoxDecoration(
+                        color: Colors.deepPurple.shade700,
+                        borderRadius: const BorderRadius.only(
+                          topLeft: Radius.circular(12),
+                          topRight: Radius.circular(12),
+                          bottomRight: Radius.circular(12),
+                        )),
+                    child: Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Text(
+                        message.content,
+                        style: const TextStyle(color: Colors.white),
+                      ),
                     ),
                   ),
                 ),
